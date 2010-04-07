b3f4604e5a9932be7821944e9403d6e69b5c1078
Guillaume Lelarge <guillaume.lelarge@dalibo.com>
2010-04-07 23:53:59 +0200

    Some fixes to make it work with pre-8.4 releases.

--- pgsnap/lib/sequences2.php
+++ pgsnap/lib/sequences2.php
@@ -27,7 +27,11 @@ if(!$g_withoutsysobjects) {
 }
 
 // Build the SQL for the real query
-$query = "SELECT relname FROM pg_class WHERE relkind='S' ORDER BY relname";
+$query = "SELECT nspname||'.'||relname AS relname
+FROM pg_class, pg_namespace
+WHERE relkind='S'
+  AND relnamespace=pg_namespace.oid
+ORDER BY relname";
 
 $rows = pg_query($connection, $query);
 if (!$rows) {
@@ -35,57 +39,68 @@ if (!$rows) {
   exit;
 }
 
-$query = '';
-while ($row = pg_fetch_array($rows)) {
-  if (strlen($query) > 0)
+if (pg_num_rows($rows) > 0)
+{
+  $query = '';
+  while ($row = pg_fetch_array($rows)) {
+    if (strlen($query) > 0)
       $query .= "\nUNION\n";
-  $query .= 'SELECT sequence_name, last_value, start_value, increment_by,
-  max_value, min_value, cache_value, log_cnt,
-  is_cycled, is_called
-FROM '.$row['relname'];
+    $query .= 'SELECT sequence_name, last_value, ';
+    if ($g_version > 83) {
+      $query .= 'start_value, ';
+    }
+    $query .= 'increment_by,
+    max_value, min_value, cache_value, log_cnt,
+    is_cycled, is_called
+  FROM '.$row['relname'];
+  }
+  $query .= ' ORDER BY sequence_name';
+  
+  $rows = pg_query($connection, $query);
+  if (!$rows) {
+    echo "An error occured.\n";
+    exit;
+  }
+  
+  $buffer .= '<div class="tblBasic">
+  
+  <table border="0" cellpadding="0" cellspacing="0" class="tblBasicGrey">
+  <tr>
+    <th class="colFirst">Sequence name</th>
+    <th class="colMid">Last Value</th>
+    <th class="colMid">Start Value Owner</th>
+    <th class="colMid">Increment By</th>
+    <th class="colMid">Max Value</th>
+    <th class="colMid">Min Value</th>
+    <th class="colMid">Cache Value</th>
+    <th class="colMid">Log Count</th>
+    <th class="colMid">Is Cycled?</th>
+    <th class="colLast">Is Called?</th>
+  </tr>
+  ';
+  
+  while ($row = pg_fetch_array($rows)) {
+  $buffer .= tr($row['schema'])."
+    <td>".$row['sequence_name']."</td>
+    <td>".$row['last_value']."</td>
+    <td>".$row['start_value']."</td>
+    <td>".$row['increment_by']."</td>
+    <td>".$row['max_value']."</td>
+    <td>".$row['min_value']."</td>
+    <td>".$row['cache_value']."</td>
+    <td>".$row['log_cnt']."</td>
+    <td>".$image[$row['is_cycled']]."</td>
+    <td>".$image[$row['is_called']]."</td>
+  </tr>";
+  }
+  $buffer .= '</table>
+  </div>
+  ';
 }
-$query .= ' ORDER BY sequence_name';
-
-$rows = pg_query($connection, $query);
-if (!$rows) {
-  echo "An error occured.\n";
-  exit;
+else
+{
+    $buffer .= "No existing sequences.";
 }
-
-$buffer .= '<div class="tblBasic">
-
-<table border="0" cellpadding="0" cellspacing="0" class="tblBasicGrey">
-<tr>
-  <th class="colFirst">Sequence name</th>
-  <th class="colMid">Last Value</th>
-  <th class="colMid">Start Value Owner</th>
-  <th class="colMid">Increment By</th>
-  <th class="colMid">Max Value</th>
-  <th class="colMid">Min Value</th>
-  <th class="colMid">Cache Value</th>
-  <th class="colMid">Log Count</th>
-  <th class="colMid">Is Cycled?</th>
-  <th class="colLast">Is Called?</th>
-</tr>
-';
-
-while ($row = pg_fetch_array($rows)) {
-$buffer .= tr($row['schema'])."
-  <td>".$row['sequence_name']."</td>
-  <td>".$row['last_value']."</td>
-  <td>".$row['start_value']."</td>
-  <td>".$row['increment_by']."</td>
-  <td>".$row['max_value']."</td>
-  <td>".$row['min_value']."</td>
-  <td>".$row['cache_value']."</td>
-  <td>".$row['log_cnt']."</td>
-  <td>".$image[$row['is_cycled']]."</td>
-  <td>".$image[$row['is_called']]."</td>
-</tr>";
-}
-$buffer .= '</table>
-</div>
-';
 
 $buffer .= '<button id="showthesource">Show SQL commands!</button>
 <div id="source">
