6cfa87f55fb7ff01c73c9bafd92c63a3aac65b41
Guillaume Lelarge <guillaume@lelarge.info>
2011-12-04 19:12:37 +0100

    Default ACL report must show the database objects menu
    It was displaying the global objects menu instead.

--- pgsnap/lib/defaultacl.php
+++ pgsnap/lib/defaultacl.php
@@ -16,7 +16,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-$buffer = $navigate_globalobjects.'
+$buffer = $navigate_dbobjects.'
 <div id="pgContentWrap">
 
 <h1>Default ACLs</h1>
