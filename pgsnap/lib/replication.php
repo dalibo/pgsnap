66f2dd35eb235785d3e0cd31dd9286c09ea8b4a4
Guillaume Lelarge <guillaume@lelarge.info>
2015-01-02 17:49:37 +0100

    Update copyright year

--- pgsnap/lib/replication.php
+++ pgsnap/lib/replication.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2008-2014 Guillaume Lelarge <guillaume@lelarge.info>
+ * Copyright (c) 2008-2015 Guillaume Lelarge <guillaume@lelarge.info>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
